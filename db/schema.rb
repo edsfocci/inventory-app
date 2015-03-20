5654dd7cd6eb4c1f852b3af7da8e4973935f396b
Ed Solis <edsfocci@gmail.com>
2015-03-19 20:35:37 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320010936) do
+ActiveRecord::Schema.define(version: 20150320012456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "compartments", force: true do |t|
+    t.integer  "item_id"
+    t.integer  "location_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.string   "name"
